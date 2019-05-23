f10be613547825abe261dd0f639ac21ed8f001c3
Steven Smethurst <ssmethurst@chipkin.com>
2019-05-23 16:25:43 -0700

    Added documentation to the MSTP call back functions

--- linuxMSTPFunctions.h
+++ linuxMSTPFunctions.h
@@ -15,10 +15,38 @@
 
 // CAS BACnet Stack MSTP callback functions
 // ----------------------------------------------------------------------------
+
+// mstp_RecvByte
+// Receives 1 byte from the UART, Non blocking
+// Return
+//   false - No bytes are avaliable on the UART.
+//   true - A byte was recived.
 bool mstp_RecvByte(uint8_t* buffer);
+
+// mstp_SendByte
+// Sends 1 or more bytes out of the UART, Non blocking
+// Return
+//   false - 1 or more of the bytes could not be sent.
+//   true - all of the bytes were sent.
 bool mstp_SendByte(uint8_t* buffer, uint32_t length);
+
+// void mstp_Sleep(uint32_t ms);
+// Used for single threaded systems. The MSTP thread is finnished with this loop. The MSTP stack
+// will not need the thread again for {ms} time and the systems other proccess can use this time
+// to do sensor readings, or other proccesses.
 void mstp_Sleep(uint32_t ms);
+
+// void mstp_TimerReset();
+// Resets the timer back to zero.
 void mstp_TimerReset();
+
+// uint32_t mstp_TimerDifference();
+// Returns the amount of microsecond (us) since the last time mstp_TimerReset() was called.
+// This timer needs a resolution of 1 millisecond (ms). If the mstp_TimerDifference()
+// function is called mulitple times without calling the mstp_TimerReset() then the value
+// returned by mstp_TimerDifference() will incurment.
+//
+// For example: If 3 millisecond elapse then This function should return 3000 us
 uint32_t mstp_TimerDifference();
 
 // Driver specific
